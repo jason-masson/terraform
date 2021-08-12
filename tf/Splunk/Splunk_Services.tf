791e2d509c86661d2f2418cb8b36bc4368cfe83b
Jason Masson <69549693+mejason83@users.noreply.github.com>
2021-08-12 11:28:00 -0400

    .

--- tf/Splunk/Splunk_Services.tf
+++ tf/Splunk/Splunk_Services.tf
@@ -1,3 +1,16 @@
+resource "nsxt_policy_service" "Between_Splunk_components" {
+   description  = "Between_Splunk_components"
+   display_name = "Between_Splunk_components"
+
+   l4_port_set_entry {
+       display_name      = "TCP-8089"
+       description       = "TCP-8089"
+       protocol          = "TCP"
+       destination_ports = ["8089"]
+   }
+
+}
+
 resource "nsxt_policy_service" "Between_Splunk_indexer_cluster_peer_nodes" {
    description  = "Between_Splunk_indexer_cluster_peer_nodes"
    display_name = "Between_Splunk_indexer_cluster_peer_nodes"
