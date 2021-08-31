01fdd9151872af773d7b0adcd7cc3a1aece0b437
Jason Masson <69549693+mejason83@users.noreply.github.com>
2021-08-31 11:18:45 -0400

    .

---
+++ tf/variables.tf
@@ -0,0 +1,15 @@
+#This file contains varibales for use in other files/configs
+
+# NSX Manager IP or FQDN
+variable "nsx_manager" {
+  default = "<IP_FQDN>"
+}
+
+# Username & Password for NSX-T Manager
+variable "username" {
+  default = "admin"
+}
+
+variable "password" {
+    default = "INSERT_PWD"
+}
