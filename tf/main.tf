692018ad1819b558698c6aa86dda4d17d55f8d05
Jason Masson <69549693+jason-masson@users.noreply.github.com>
2021-10-27 16:50:02 -0400

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -13,7 +13,7 @@ provider "nsxt" {
   username              = var.username
   password              = var.password
   allow_unverified_ssl  = true
-   max_retries           = 10
+   max_retries          = 10
   retry_min_delay       = 500
   retry_max_delay       = 5000
   retry_on_status_codes = [429]
