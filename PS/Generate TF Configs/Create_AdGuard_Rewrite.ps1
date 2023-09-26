45c2955c0a67bac029c5881eacdff8481da24acb
Jason Masson <69549693+jason-masson@users.noreply.github.com>
2023-09-26 11:30:28 -0400

    ag

--- PS/Generate TF Configs/Create_AdGuard_Rewrite.ps1
+++ PS/Generate TF Configs/Create_AdGuard_Rewrite.ps1
@@ -3,20 +3,31 @@
 #clean console before running
 Clear-Host
 
+#Timestamp Function
+function Get-Timestamp {
+    Get-Date -Format HH:mm:ss.fff
+}
+
 #Variables Section
 #Import tag data from the file below
-$csvfile = "/home/jason/Documents/DataFiles/AdGuardRewrite.csv"
+$csvfile = "/filepath/AdGuardRewrite.csv"
 #Where to save the generated config
-$OutputFile = "/home/jason/Documents/GitHub/terraform/tf/Adguard/AdGuardRewrite.tf"
+$OutputFile = "/filepath/AdGuardRewrite.tf"
 
-#Import the CSV file, break out the VM, UUID, and tags then loop thru each
+#Stamp start time
+Write-Host -ForegroundColor Red "Start time is $(Get-Timestamp)"
+Add-Content $OutputFile "#Start time is $(Get-Timestamp)"
+Add-Content $OutputFile ""
+
+#Import the CSV file, break out the domain and answer tags then loop thru each
 foreach ($data in (Import-Csv -Path $csvfile)) {
     $domain = $data.domain
     $answer = $data.answer
+    $name = $data.name
         
     #Section for generating the configs
-    #Fiddling with this section will likely break the config
-    Add-Content $OutputFile "resource `"adguard_rewrite`" `"$($domain)`" {"
+    #Fiddling with this section will likely break the config file itself
+    Add-Content $OutputFile "resource `"adguard_rewrite`" `"_$($name)`" {"
     Add-Content $OutputFile "   domain = `"$($domain)`""
     Add-Content $OutputFile "   answer = `"$($answer)`""
     Add-Content $OutputFile "}"
@@ -26,7 +37,9 @@ foreach ($data in (Import-Csv -Path $csvfile)) {
     Write-Host -ForegroundColor Green "Created AdGguard Rewrite Config for $($domain) = $($answer)"
 }
 
-Write-Host -ForegroundColor Red "Finished!, Results saved to $($OutputFile)"
+#Stamp end  time
+Write-Host -ForegroundColor Red "End time is $(Get-Timestamp)"
+Add-Content $OutputFile "#End time is $(Get-Timestamp)"
 
 #Garbage Collect to clear variables before the next run
 [System.GC]::Collect()
